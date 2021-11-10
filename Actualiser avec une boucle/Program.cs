38c1e4af10fce3c5aefce60790102fccf50a2f21
Théo GIRANDOLA <tt.girandola@gmail.com>
2021-11-10 16:57:02 +0100

    oui

---
+++ Actualiser avec une boucle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Actualiser_avec_une_boucle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to stop");
+          
+            
+        }
+    }
+}
