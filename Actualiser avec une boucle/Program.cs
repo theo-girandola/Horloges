f47669996493aff0d508b4d3fe00157cd117cf4c
Théo GIRANDOLA <tt.girandola@gmail.com>
2021-11-12 15:57:25 +0100

    ah

--- Actualiser avec une boucle/Program.cs
+++ Actualiser avec une boucle/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+using System.Timers;
 
-namespace Actualiser_avec_une_boucle
+public class Example
 {
-    class Program
+    private static System.Timers.Timer aTimer;
+
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Press any key to stop");
-          
-            
-        }
+        SetTimer();
+
+        Console.WriteLine("\nPress the Enter key to exit the application...\n");
+        Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+        Console.ReadLine();
+        aTimer.Stop();
+        aTimer.Dispose();
+
+        Console.WriteLine("Terminating the application...");
+    }
+
+    private static void SetTimer()
+    {
+        // Create a timer with a two second interval.
+        aTimer = new System.Timers.Timer(1000);
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                          e.SignalTime);
     }
 }
--- ClearConsole/Program.cs
+++ ClearConsole/Program.cs
@@ -1,14 +1,55 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
 
 namespace ClearConsole
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-           
-            Console.WriteLine("Hello World!"); 
-            Console.Clear();
+       
+            
+       
+            public static void Main()
+            {
+                try
+                {
+                    using (Process myProcess = Process.Start("Notepad.exe"))
+                    {
+                        // Display physical memory usage 5 times at intervals of 2 seconds.
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if (!myProcess.HasExited)
+                            {
+                                // Discard cached information about the process.
+                                myProcess.Refresh();
+                                // Print working set to console.
+                                Console.WriteLine($"Physical Memory Usage: {myProcess.WorkingSet}");
+                                // Wait 2 seconds.
+                                Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        // Close process by sending a close message to its main window.
+                        myProcess.CloseMainWindow();
+                        // Free resources associated with process.
+                        myProcess.Close();
+                    }
+                }
+                catch (Exception e) when (e is Win32Exception || e is FileNotFoundException)
+                {
+                    Console.WriteLine("The following exception was raised: ");
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
-    }
+    
 }
+   
+
